3849eac0984440359d08b8bd029d28b65c2cf74e
EyosiyasMekbib <eyosi9000@gmail.com>
2026-03-11 23:13:11 +0300

    style: apply new color palette to web app

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
         }],
     ],
 
-    css: ['@world-bingo/ui/styles/tokens.css'],
+    css: ['@world-bingo/ui/styles/tokens.css', '~/assets/css/theme.css'],
 
     runtimeConfig: {
         jwtSecret: '',
